00ed37df834d1c17500ad54f9a1d8bb7565b88b1
Tsuchiya Kanm <150462533+kttsu@users.noreply.github.com>
2025-02-17 06:48:58 +0900

    不要なコメントアウトを削除

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,13 +2,11 @@ import axios from "axios";
 
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
 
-// 全ライブを取得
 export const fetchLives = async () => {
   const response = await axios.get(`${API_BASE_URL}/live`);
   return response.data;
 };
 
-// 新しいライブを登録
 export const createLive = async (liveData) => {
   const response = await axios.post(`${API_BASE_URL}/live`, liveData, {
     headers: { "Content-Type": "application/json" },
@@ -16,12 +14,10 @@ export const createLive = async (liveData) => {
   return response.data;
 };
 
-// 指定したライブを削除
 export const deleteLive = async (id) => {
   await axios.delete(`${API_BASE_URL}/live/${id}`);
 };
 
-// 指定したライブを更新
 export const updateLive = async (id, updatedLiveData) => {
     const response = await axios.patch(`${API_BASE_URL}/live/${id}`, updatedLiveData, {
       headers: { "Content-Type": "application/json" },
